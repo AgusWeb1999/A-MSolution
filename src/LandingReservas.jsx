1a4469c77f874e2ee3b06c0e49ddbd3d5eecc23a
agusmazzini <agumazzini@hotmail.com>
2025-07-27 22:49:05 -0300

    chore: cambio mínimo para forzar deploy

--- src/LandingReservas.jsx
+++ src/LandingReservas.jsx
@@ -41,6 +41,7 @@ function LandingReservas({ goHome }) {
               <h3>Escalable y Seguro</h3>
               <p>Ideal para cualquier comercio, con soporte y actualizaciones.</p>
             </div> 
+            {/* Cambio mínimo para forzar deploy */}
           </div>
         </section>
         <section className="testimonios-section">
